869dd00971b128ccfc5a1435fad38694832cb160
gonzashima <gonza.shima@gmail.com>
2023-05-23 12:40:37 -0300

    detalles

--- src/main/java/Modelo/Utils/ModificarDB.java
+++ src/main/java/Modelo/Utils/ModificarDB.java
@@ -1,10 +1,7 @@
 package Modelo.Utils;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-@Deprecated
 public class ModificarDB {
     public void modificarDB() throws SQLException {
         ConectorDB.getConnection();

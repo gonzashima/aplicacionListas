557607c2c43fcde644f2f74ab653f23e6c20ccb1
gonzashima <gonza.shima@gmail.com>
2023-12-26 20:59:59 -0300

    se refactorizaron los insertadores

--- src/main/java/Modelo/Insertadores/Insertador.java
+++ src/main/java/Modelo/Insertadores/Insertador.java
@@ -2,23 +2,45 @@ package Modelo.Insertadores;
 
 import Modelo.Constantes.ConstantesNumericas;
 import Modelo.Productos.Producto;
+import Modelo.Utils.ConectorDB;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public abstract class Insertador {
+public class Insertador {
     /**
      * Toma los productos y los inserta o los actualiza en la DB, segun corresponda
      * */
     protected static int CODIGO_NUEVOS = 0;
     protected static int CODIGO_ACTUALIZAR = 1;
 
+    private final String nombreLista;
 
-    //TODO habria que hacer un refactor de los insertadores. InsertarABaseDeDatos es basicamente el mismo codigo en todas las clases hijas, solo cambia el codigo. El unico diferente
-    //TODO es mafersa que hace lo mismo pero en loop
-    public abstract void insertarABaseDeDatos(HashMap<Integer, HashMap<Integer, Producto>> datos) throws SQLException;
+    public Insertador(String nombreLista) {
+        this.nombreLista = nombreLista;
+    }
 
-    public HashMap<Integer, HashMap<Integer, Producto>> separarProductos(HashMap<Integer, Producto> productos) {
+
+    public void insertarABaseDeDatos(HashMap<Integer, HashMap<Integer, Producto>> datos) throws SQLException {
+        ConectorDB.getConnection();
+        int codigo = ConstantesNumericas.codigoLista(nombreLista);
+
+        HashMap<Integer, Producto> productos = datos.get(codigo);
+        HashMap<Integer, HashMap<Integer, Producto>> resultado = separarProductos(productos);
+
+        HashMap<Integer, Producto> productosNuevos = resultado.get(CODIGO_NUEVOS);
+        HashMap<Integer, Producto> productosActualizar = resultado.get(CODIGO_ACTUALIZAR);
+
+        List<Producto> paraActualizar = new ArrayList<>(productosActualizar.values());
+
+        ConectorDB.guardarCambios(paraActualizar);
+        ConectorDB.insertarProcuctos(productosNuevos, codigo);
+        datos.replace(codigo, ConectorDB.seleccionarProductos(nombreLista));
+    }
+
+    public static HashMap<Integer, HashMap<Integer, Producto>> separarProductos(HashMap<Integer, Producto> productos) {
         HashMap<Integer, Producto> productosNuevos = new HashMap<>();
         HashMap<Integer, Producto> productosActualizar = new HashMap<>();
 
